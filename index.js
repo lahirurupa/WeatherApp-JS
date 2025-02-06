82a0805e3bb3c8b599b163c240c0038a765ed903
lahirurupa <lahirurupasinghe2001@gmail.com>
2025-02-06 14:13:37 +0530

    Add display functions

--- index.js
+++ index.js
@@ -39,12 +39,54 @@ async function getWeatherData(city) {
 
 // Function to display weather data
 function displayWeatherData(data) {
-    console.log(data);
+    
+    const { name: city, main: {temp, humidity}, weather: [{description, id}] } = data;
+
+    card.textContent = "";
+    card.style.display = 'flex';
+
+    const cityDisplay = document.createElement('h1');
+    const tempDisplay = document.createElement('p');
+    const humidityDisplay = document.createElement('p');
+    const descDisplay = document.createElement('p');
+    const weatherEmoji = document.createElement('p');
+
+    cityDisplay.textContent = city;
+    tempDisplay.textContent = `Temperature: ${temp}°C`;
+    humidityDisplay.textContent = `Humidity: ${humidity}%`;
+    descDisplay.textContent = description;
+    weatherEmoji.textContent = getWeatherEmoji(id);
+
+    cityDisplay.classList.add('cityDisplay');
+    tempDisplay.classList.add('tempDisplay');
+    humidityDisplay.classList.add('humidityDisplay');
+    descDisplay.classList.add('descDisplay');
+    weatherEmoji.classList.add('weatherEmoji');
+
+    card.appendChild(cityDisplay);
+    card.appendChild(tempDisplay);
+    card.appendChild(humidityDisplay);
+    card.appendChild(descDisplay);
+    card.appendChild(weatherEmoji);
 }
 
 // Function to get weather emoji
-function getWeatherEmoji(weather) {
-
+function getWeatherEmoji(weatherId) {
+    if (weatherId >= 200 && weatherId < 300) {
+        return '⛈️';
+    } else if (weatherId >= 300 && weatherId < 500) {
+        return '🌧️';
+    } else if (weatherId >= 500 && weatherId < 600) {
+        return '🌧️';
+    } else if (weatherId >= 600 && weatherId < 700) {
+        return '❄️';
+    } else if (weatherId >= 700 && weatherId < 800) {
+        return '🌫️';
+    } else if (weatherId === 800) {
+        return '☀️';
+    } else if (weatherId > 800) {
+        return '☁️';
+    }
 }
 
 // Function to display error message
